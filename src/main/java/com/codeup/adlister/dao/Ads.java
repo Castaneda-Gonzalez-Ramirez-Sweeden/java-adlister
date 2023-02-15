0b5815ec8557a1833656d1a5957d5c15ed56b9d3
Javier Castaneda <castaneda.javier.j@gmail.com>
2023-02-15 09:33:57 -0600

    adlister project

--- src/main/java/com/codeup/adlister/dao/Ads.java
+++ src/main/java/com/codeup/adlister/dao/Ads.java
@@ -11,10 +11,14 @@ public interface Ads {
     // insert a new ad and return the new ad's id
     Long insert(Ad ad);
 
+    // used to delete an ad.
+    void deleteAdById(int id);
+
+    Ad getAdById (int id);
+
+
     List<Ad> searchAds (String searchString) throws SQLException;
 
-    void deleteAd(Ad ad);
 
-    Ad getAdById(int id);
 
 }
