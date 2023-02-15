8f15a96ae45ff2f5c484a03bee1fb4e3c6f7c349
Nicole Sweeden <sweeden.nicole@gmail.com>
2023-02-15 10:58:13 -0700

    saving my changes to edit ad servlet

--- src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
+++ src/main/java/com/codeup/adlister/controllers/EditAdServlet.java
@@ -1,71 +1,69 @@
-//package com.codeup.adlister.controllers;
-//
-//import com.codeup.adlister.dao.DaoFactory;
-//import com.codeup.adlister.models.Ad;
-//import com.codeup.adlister.models.User;
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//import java.sql.SQLException;
-//import java.util.Date;
-//
-////public class EditAdServlet extends HttpServlet {
-////
-////    @Override
-////    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-////        super.doGet(req, resp);
-////    }
-////
-////    @Override
-////    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-////        super.doPost(req, resp);
-////    }
-////}
-//
-//@WebServlet(name = "EditAdServlet", urlPatterns = "/edit-ad/*")
+package com.codeup.adlister.controllers;
+
+import com.codeup.adlister.dao.DaoFactory;
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.User;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Date;
+
 //public class EditAdServlet extends HttpServlet {
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        if (request.getSession().getAttribute("user") == null) {
-//            response.sendRedirect("/error");
-//        } else  {
-//            int AdId = Integer.parseInt(request.getParameter("AdId"));
 //
-//            try {
-//                request.setAttribute("ad", DaoFactory.getCarsDao().getCarById(carId));
-//            } catch (SQLException e) {
-//                e.printStackTrace();
-//            }
-//            request.getRequestDispatcher("/WEB-INF/cars/edit-car.jsp").forward(request,response);
-//        }
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
 //    }
 //
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-//        int carId = Integer.parseInt(request.getParameter("editId"));
-//        User user = DaoFactory.getUsersDao().findByUsername((String) request.getSession().getAttribute("user"));
-//
-//        String price = request.getParameter("price");
-//        double doublePrice = Double.parseDouble(price.replaceAll(",", ""));
-//
-//        Car car = new Car(
-//                carId,
-//                user.getId(),
-//                Integer.parseInt(request.getParameter("year")) ,
-//                request.getParameter("make"),
-//                request.getParameter("model"),
-//                doublePrice,
-//                request.getParameter("description"),
-//                new Date()
-//        );
-//
-//        try {
-//            DaoFactory.getCarsDao().updateCar(car); // Updates car in DB
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//        response.sendRedirect("/profile");
+//    @Override
+//    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doPost(req, resp);
 //    }
 //}
+
+@WebServlet(name = "EditAdServlet", urlPatterns = "/edit-ad/*")
+public class EditAdServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        if (request.getSession().getAttribute("user") == null) {
+            response.sendRedirect("/error");
+        } else  {
+            int AdId = Integer.parseInt(request.getParameter("AdId"));
+
+            try {
+                request.setAttribute("ad", DaoFactory.getAdsDao().getAdById(AdId));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            request.getRequestDispatcher("/WEB-INF/ads/edit-ad.jsp").forward(request,response);
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int AdId = Integer.parseInt(request.getParameter("editId"));
+        User user = DaoFactory.getUsersDao().findByUsername((String) request.getSession().getAttribute("user"));
+
+        String price = request.getParameter("price");
+        double doublePrice = Double.parseDouble(price.replaceAll(",", ""));
+
+        Ad ad = new ad( //refactored - nicole
+                AdId,
+                user.getId(),
+//                Integer.parseInt(request.getParameter("year")) ,
+                request.getParameter("title"),
+                request.getParameter("description"),
+//                new Date()
+        );
+
+        try {
+            DaoFactory.getAdsDao().updateAd(ad); // Updates ad in DB -nicole
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        response.sendRedirect("/profile");
+    }
+}
